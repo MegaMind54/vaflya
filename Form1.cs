900072954e7b4f5285d6e4364b6d87b799097b75
MegaMind54 <152641664+MegaMind54@users.noreply.github.com>
2023-12-01 20:15:15 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,23 @@
+﻿using SAPPER.Controllers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SAPPER
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            MapController.Init(this);
+        }
+    }
+}
